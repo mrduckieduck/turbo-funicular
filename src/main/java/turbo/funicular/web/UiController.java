37a01516c6a69bd6dd43adb396edfe54e2178200
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-08 17:04:50 -0600

    Agrega el estilo inicial closes #6

---
+++ src/main/java/turbo/funicular/web/UiController.java
@@ -0,0 +1,17 @@
+package turbo.funicular.web;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.View;
+
+import java.util.Map;
+
+@Controller
+public class UiController {
+    @View("index")
+    @Get
+    public HttpResponse<Map> index() {
+        return HttpResponse.ok(Map.of());
+    }
+}
