644af584f983924372f40823d0bb3f0ca44b0b94
Daniel Castillo <danielmcasv@gmail.com>
2020-11-15 23:21:27 -0600

    Configuracion de autenticacion minima

--- src/main/java/turbo/funicular/web/UiController.java
+++ src/main/java/turbo/funicular/web/UiController.java
@@ -3,18 +3,23 @@ package turbo.funicular.web;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.View;
 
 import java.util.Map;
 
 @Controller
 public class UiController {
+
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @View("index")
     @Get
     public HttpResponse<Map> index() {
         return HttpResponse.ok(Map.of());
     }
 
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @View("start")
     @Get("/getting-started")
     public HttpResponse<Map> start() {
