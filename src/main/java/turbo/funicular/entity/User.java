b8ee5b802efcb2d44dbd9700c33e3064d3042d33
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-15 22:06:31 -0600

    Agrega el servicio de validacion y anotaciones de Bean Validation #18

--- src/main/java/turbo/funicular/entity/User.java
+++ src/main/java/turbo/funicular/entity/User.java
@@ -1,5 +1,6 @@
 package turbo.funicular.entity;
 
+import io.micronaut.core.annotation.Introspected;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,19 +9,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Setter
 @Getter
 @Table(name = "users")
+@Introspected
 public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank
+    @Size(max = 200)
     private String login;
+    @Size(max = 2000)
     private String avatarUrl;
+    @Size(max = 2000)
     private String bio;
+    @NotNull
     private Long ghId;
+    @Size(max = 200)
     private String name;
     @Column(name = "public_gists")
     private Integer publicGistsCount;
