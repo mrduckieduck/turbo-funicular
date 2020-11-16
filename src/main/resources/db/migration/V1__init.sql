279c6d22054203f38c5e1f215bc8de82a360412e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-15 19:18:12 -0600

    Clases iniciales para almacenar los datos de los usuarios en Postgres #16

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users
+(
+    id       SERIAL,
+    username VARCHAR(200)
+);
--- src/test/groovy/turbo/funicular/TurboFunicularSpec.groovy
+++ src/test/groovy/turbo/funicular/TurboFunicularSpec.groovy
@@ -3,17 +3,33 @@ package turbo.funicular
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
+import turbo.funicular.entity.User
+import turbo.funicular.entity.UserRepository
+
 import javax.inject.Inject
+import javax.transaction.Transactional
 
 @MicronautTest
+@Transactional
 class TurboFunicularSpec extends Specification {
 
     @Inject
     EmbeddedApplication<?> application
 
+    @Inject
+    UserRepository userRepository
+
     void 'test it works'() {
         expect:
-        application.running
+            application.running
+    }
+
+    void 'ssss'() {
+        given:
+            def user = new User(username: 'domix')
+            userRepository.save(user)
+        expect:
+            userRepository.findAll().size() == 1
     }
 
 }
