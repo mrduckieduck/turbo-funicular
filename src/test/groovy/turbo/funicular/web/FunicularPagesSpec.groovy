f262e5c0648cc4318d4eebda20c4df2120c06af9
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-16 23:40:13 -0600

    Corrige las pruebas #20

--- src/test/groovy/turbo/funicular/web/FunicularPagesSpec.groovy
+++ src/test/groovy/turbo/funicular/web/FunicularPagesSpec.groovy
@@ -54,7 +54,7 @@ class StartPage extends Page {
 
 class UserHomePage extends Page {
     static url = "/home"
-    static at = { title == "User Home" }
+    static at = { title == "Home" }
     static content = {
     }
 }
