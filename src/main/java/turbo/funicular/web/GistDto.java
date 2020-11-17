000ad6dfc15106a337bfc79763c16a2a6264635f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-16 21:45:24 -0600

    Remueve anotaciones no usadas #20

--- src/main/java/turbo/funicular/web/GistDto.java
+++ src/main/java/turbo/funicular/web/GistDto.java
@@ -1,25 +1,19 @@
 package turbo.funicular.web;
 
 import io.micronaut.core.annotation.Introspected;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Introspected
 public class GistDto {
-    private Integer commentsCount;
-    private LocalDateTime createdAt;
-    private String description;
-    private String ghId;
-    private LocalDateTime updatedAt;
-    private Boolean publicGist;
+    private final Integer commentsCount;
+    private final LocalDateTime createdAt;
+    private final String description;
+    private final String ghId;
+    private final LocalDateTime updatedAt;
+    private final Boolean publicGist;
 }
