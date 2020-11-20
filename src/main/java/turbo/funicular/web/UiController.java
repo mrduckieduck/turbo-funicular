927583c4711344a0f75ead6f49e4225d39649a44
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-20 00:52:54 -0600

    Handles the logic for auth or anon users to render the proper views #20

--- src/main/java/turbo/funicular/web/UiController.java
+++ src/main/java/turbo/funicular/web/UiController.java
@@ -4,17 +4,28 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
 import io.micronaut.views.View;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import turbo.funicular.service.GitHubService;
 import turbo.funicular.service.UsersService;
 
+import javax.annotation.Nullable;
+import java.net.URI;
+import java.security.Principal;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS;
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
 import static turbo.funicular.service.UsersMapper.USERS_MAPPER;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UiController {
@@ -24,35 +35,51 @@ public class UiController {
     @Get
     @View("index")
     @Secured(IS_ANONYMOUS)
-    public HttpResponse<Map> index() {
-        return HttpResponse.ok(Map.of());
+    public HttpResponse index() {
+        return HttpResponse.ok();
     }
 
+    @SneakyThrows
     @View("start")
     @Get("/getting-started")
     @Secured(IS_ANONYMOUS)
-    public HttpResponse<Map> start() {
-        return HttpResponse.ok(Map.of());
+    public HttpResponse start(@Nullable Authentication authentication) {
+        if (Objects.nonNull(authentication)) {
+            //if the user is authenticated, sent to home
+            return HttpResponse.redirect(new URI("/home"));
+        }
+        return HttpResponse.ok();
     }
 
     @Get("/home")
     @View("user_home")
-    @Secured(IS_ANONYMOUS)
-    public HttpResponse<Map> home() {
+    @Secured(IS_AUTHENTICATED)
+    public HttpResponse home(Authentication authentication) {
+        return HttpResponse.ok(model(authentication));
+    }
+
+    private Map<String, Object> model(Authentication authentication) {
         final var users = usersService.randomTop(5)
             .stream()
             .map(USERS_MAPPER::entityToCommand)
             .collect(Collectors.toList());
 
-        //TODO: This should be set by the security infra when ready. to get the gists owned by the current user logged
-        final String username = "";
+        final var username = Optional.ofNullable(authentication)
+            .map(Principal::getName)
+            .orElse("");
 
-        final var model = Map.of(
+        final var roles = Optional.ofNullable(authentication)
+            .map(auth -> auth.getAttributes().get("roles"))
+            .map(strings -> (List<String>) strings)
+            .orElse(List.of());
+
+        return Map.of(
+            "isLoggedIn", Objects.nonNull(authentication),
+            "username", username,
             "featuredUsers", users,
-            "gists", gitHubService.findGistsByUser(username)
+            "gists", gitHubService.findGistsByUser(username),
+            "roles", roles
         );
-
-        return HttpResponse.ok(model);
     }
 
 }
