bc519e4d96896409d61d5e5ef4ab914dcccaf452
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-21 22:39:07 -0600

    enhances tests for UserDetailsMapper #54

--- src/test/groovy/turbo/funicular/security/UserDetailsMapperSpecs.groovy
+++ src/test/groovy/turbo/funicular/security/UserDetailsMapperSpecs.groovy
@@ -1,6 +1,6 @@
 package turbo.funicular.security
 
-
+import com.github.javafaker.Faker
 import io.micronaut.security.oauth2.endpoint.token.response.TokenResponse
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.reactivex.Flowable
@@ -14,6 +14,7 @@ import javax.inject.Inject
 class UserDetailsMapperSpecs extends Specification {
     @Inject
     UserDetailsMapper userDetailsMapper
+    Faker faker = new Faker()
 
     def 'should test the failed cases'() {
         when:
@@ -34,15 +35,17 @@ class UserDetailsMapperSpecs extends Specification {
 
     def 'should build the userDetails'() {
         given:
+            def username = faker.name().username()
             def github = Stub(GitHub)
             def myself = Stub(GHMyself)
             myself.getId() >> 1l
-            myself.getLogin() >> 'foo'
+            myself.getLogin() >> username
 
             github.getMyself() >> myself
             def accessToken = 'fooo'
             def details = userDetailsMapper.buildDetails(github, accessToken)
         expect:
-            details
+            details.username == username
+            details.getRoles() == UserDetailsMapper.ROLES
     }
 }
