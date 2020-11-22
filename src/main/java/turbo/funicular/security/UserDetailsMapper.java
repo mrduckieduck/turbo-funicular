72cb27a185e8efee5e565d11aac78404f7ff0777
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-11-21 22:54:51 -0600

    fixes failed tests for UserDetailsMapper #54

--- src/main/java/turbo/funicular/security/UserDetailsMapper.java
+++ src/main/java/turbo/funicular/security/UserDetailsMapper.java
@@ -62,7 +62,7 @@ public class UserDetailsMapper implements OauthUserDetailsMapper {
     @SneakyThrows
     protected UserDetails buildDetails(GitHub github, String accessToken) {
         final var user = github.getMyself();
-        final var id = user.getId();
+        final long id = user.getId();
         final var username = user.getLogin();
 
         final var userCommand = USERS_MAPPER.githubToCommand(user);
