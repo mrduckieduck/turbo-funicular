00faed7b4c96d891ed87f39a7b33a81ab72cf8a7
Daniel Castillo <danielmcasv@gmail.com>
2020-11-22 01:27:00 -0600

    Prueba de controlador de UI

--- src/test/groovy/turbo/funicular/web/UiControllerSpecs.groovy
+++ src/test/groovy/turbo/funicular/web/UiControllerSpecs.groovy
@@ -2,6 +2,7 @@ package turbo.funicular.web
 
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.kohsuke.github.GHUser
 import spock.lang.Specification
 import turbo.funicular.service.GitHubService
 import turbo.funicular.service.UsersService
@@ -17,7 +18,9 @@ class UiControllerSpecs extends Specification {
 
     def 'should test the model generation for auth user at home page'() {
         given:
+            def ghUser = Stub(GHUser)
             def authentication = Stub(Authentication)
+            authentication.attributes.put('ghUser', ghUser)
             def controller = new UiController(usersService, gitHubService)
         when:
             def model = controller.model(authentication)
@@ -27,7 +30,10 @@ class UiControllerSpecs extends Specification {
 
     def 'should test the start page for null auth'() {
         given:
+            def ghUser = Stub(GHUser)
             def authentication = Stub(Authentication)
+            authentication.attributes.put('ghUser', ghUser)
+
             def controller = new UiController(usersService, gitHubService)
         when:
             def response = controller.start(authentication)
