6dd1a94639897aa600c97f8e20962c8830a08b68
Daniel Castillo <danielmcasv@gmail.com>
2020-11-21 20:33:26 -0600

    Rompi el branch - github api client primera operaciones

---
+++ src/test/groovy/turbo/funicular/service/GithubApiClientSpecs.groovy
@@ -0,0 +1,29 @@
+package turbo.funicular.service
+
+
+import spock.lang.Specification
+
+class GithubApiClientSpecs extends Specification {
+
+    GithubApiClient githubApiClient = new GithubApiClient()
+
+    void 'Test github operations for user #login'() {
+        when:
+            def user = githubApiClient.getUser(login)
+        then: 'Check user info'
+            user.present
+            with(user.get()) {
+                println(it)
+                it.login == login
+                it.avatarUrl.contains(ghId.toString())
+                it.name == expectedName
+                it.ghId
+                it.publicGistsCount
+            }
+        where:
+            login          | expectedName
+            'mrduckieduck' | 'Daniel Castillo'
+            'domix'        | 'Domingo Suarez Torres'
+    }
+
+}
