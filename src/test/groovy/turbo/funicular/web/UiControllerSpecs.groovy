05864dfed9e2c620150332d2b8d93fcf6222c042
Daniel Castillo <danielmcasv@gmail.com>
2020-11-22 19:40:02 -0600

    Agreando más usuarios

--- src/test/groovy/turbo/funicular/web/UiControllerSpecs.groovy
+++ src/test/groovy/turbo/funicular/web/UiControllerSpecs.groovy
@@ -9,7 +9,6 @@ import turbo.funicular.service.GitHubService
 import turbo.funicular.service.UsersService
 
 import javax.inject.Inject
-import java.time.LocalDateTime
 
 @MicronautTest
 class UiControllerSpecs extends Specification {
@@ -26,13 +25,14 @@ class UiControllerSpecs extends Specification {
             def controller = new UiController(usersService, gitHubService)
 
         and: 'Inserting couple dummy users'
-            def user = UserCommand.builder()
-                .ghId(2L)
-                .name('user name')
-                .login('foo-login')
-                .build()
-
-            usersService.addUser(user)
+            ['username', 'foo-username'].eachWithIndex { username, idx ->
+                def userCommand = UserCommand.builder()
+                    .ghId(idx as long)
+                    .name(username)
+                    .login("$username-login")
+                    .build()
+                usersService.addUser(userCommand)
+            }
 
         expect:
             controller.model(authentication)
